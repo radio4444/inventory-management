d620ab8c888312edea1a5f8870f2c39bdc39fd19
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-10-16 12:03:58 -0400

    Implement inventory fetching from Firestore and update state in useEffect

--- app/page.js
+++ app/page.js
@@ -1,35 +1,53 @@
-'use client'
+'use client';
 
 import {useState, useEffect} from "react";
 import {Box, Stack, Typography, Button, Modal, TextField} from "@mui/material";
 import {firestore} from "@/app/firebase";
-import {collection, doc, getDocs, query, setDoc, deleteDoc, getDoc} from 'firebase/firestore'
+import {collection, doc, getDocs, query, setDoc, deleteDoc, getDoc} from 'firebase/firestore';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'white',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  display: 'flex',
-  flexDirection: 'column',
-  gap: 3,
-}
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'white',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 3,
+};
 
-export default function Home(){
-  const [inventory, setInventory] = useState([])
-  const [open, setOpen] = useState(false)
-  const [itemName, setItemName] = useState('')
 
-  return (
-      <Box>
-        <Typography variant = "h1">Inventory Management</Typography>
-      </Box>
-  )
+
+export default function Home() {
+    const [inventory, setInventory] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [itemName, setItemName] = useState('');
+
+    //Update Inventory function
+    const updateInventory = async () => {
+        const snapshot = query(collection(firestore, 'inventory'));
+        const docs = await getDocs(snapshot);
+        const inventoryList = [];
+        docs.forEach((doc) => {
+            inventoryList.push({name: doc.id, ...doc.data()})
+            setInventory(inventoryList)
+        });
+    };
+    useEffect(() => {
+        updateInventory()
+    }, []);
+
+
+
+    return (
+        <Box>
+            <Typography variant="h1">Inventory Management</Typography>
+        </Box>
+    );
 }
 
 
