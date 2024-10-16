7cd6b8d3e06fb46f75e31a846f6306ea65ea9439
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-10-16 11:22:51 -0400

    Define modal styling for centered display and layout

--- app/page.js
+++ app/page.js
@@ -5,3 +5,19 @@ import {Box, Stack, Typography, Button, Modal, TextField} from "@mui/material";
 import {firestore} from "@/app/firebase";
 import {collection, doc, getDocs, query, setDoc, deleteDoc, getDoc} from 'firebase/firestore'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+}
+
+
